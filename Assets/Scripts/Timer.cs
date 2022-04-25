18b63a7e01450376e6b717f454b1a2a16f8219b4
Oli <1024958@student.sae.edu.au>
2022-04-25 21:27:45 +1000

    replace text with text mesh pro

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] float timeValue = 90f;
-    [SerializeField] Text timeText;
+    [SerializeField] TextMeshProUGUI timeText;
 
     // Start is called before the first frame update
     void Start()
